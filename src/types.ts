1348aae22be0f49619924a3f580094b3919f8b0e
Devin Abbott <devinabbott@gmail.com>
2021-11-30 13:31:15 -0800

    Metadata type defaults to void

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
-export type File<Data, Metadata> = {
+export type File<Data, Metadata = void> = {
   readonly type: 'file'
   readonly data: Data
   readonly metadata: Metadata
 }
 
-export type Directory<Data, Metadata> = {
+export type Directory<Data, Metadata = void> = {
   readonly type: 'directory'
   readonly children: Record<string, Node<Data, Metadata>>
   readonly metadata: Metadata
 }
 
-export type Node<Data, Metadata> =
+export type Node<Data, Metadata = void> =
   | File<Data, Metadata>
   | Directory<Data, Metadata>
 
-export type Entry<Data, Metadata> = [string, Node<Data, Metadata>]
+export type Entry<Data, Metadata = void> = [string, Node<Data, Metadata>]
